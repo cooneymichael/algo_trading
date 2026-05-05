207316ea1a4ba65455e3324640c1a212065a19e4
Michael <MC509119@ohio.edu>
2026-05-04 20:39:35 -0400

    main: began work on random_walk, needs to be restructured (issue 3 on github)

--- mcmc.py
+++ mcmc.py
@@ -41,6 +41,17 @@ def get_security_history(ticker: str, start: int=None) -> pd.DataFrame:
 
 
 def calculate_markov_chain(data: pd.DataFrame) -> list[list[float]]:
+    '''Calculate a markov chain probability matrix/state diagram using the data parameter.
+    The result will have the form:
+    -------------
+    |g->g |g->l |
+    |     |     |
+    |-----|-----|
+    |l->g |l->l |
+    |     |     |
+    -------------
+    Where g is a gain and l is a loss
+    '''
     # markov chain as matrix
     chain = [[0, 0],[0, 0]]
     gain_counter = 0
@@ -88,14 +99,43 @@ def calculate_markov_chain(data: pd.DataFrame) -> list[list[float]]:
     return chain
 
 
-def random_walk(markov_chain: list[list[float]]) -> list[float]:
+def random_walk(markov_chain: list[list[float]], depth: int, initial_state: int) -> list[float]:
     '''Average a list of points over thousands of random walks.  Points should
-    be real numbers. Return the averaged list to be visualized or analyzed.'''
-    return
+    be real numbers. Return the averaged list to be visualized or analyzed.
+
+    key word arguments:
+    markov_chain --  a 2x2 matrix holding probabilities for gains and losses
+    depth --         the number of future events to simulate
+    initial_state -- a pointer to a row in markov_chain. 0 represents a gain, 1 represents a loss
+    '''
+
+    # points = []
+    simulations = np.zeros((1000, depth))
+
+    for idx, _ in enumerate(simulations):
+        state_pointer = initial_state
+        counter = 0
+
+        for i in range(depth):
+            new_state = np.random.choice([0,1], p=markov_chain[state_pointer])
+            if state_pointer != new_state:
+                state_pointer = new_state
+
+            if new_state:
+                counter += 1
+            else:
+                counter -= 1
+
+            simulations[idx][i] = counter
+
+    return simulations
 
 if __name__ == '__main__':
     # ewy_history = get_security_history('EWY', 2012)
     # print(ewy_history)
 
-    mc = calculate_markov_chain('a')
-    print(mc)
+    # mc = calculate_markov_chain('a')
+    # print(mc)
+
+    pts = random_walk([[2/3,1/3],[3/4, 1/4]], 1000, 0)
+    print(pts)
