aa0ae6e7ff468fc96c7ce64f0afd3de9a305f6a7
Michael <cooneymichael@outlook.com>
2026-05-24 20:49:45 -0400

    ORG-0: reorganized code into multiple files, directories.

--- tests/test_security.py
+++ tests/test_security.py
@@ -1,31 +1,22 @@
+################################################################################
+#
+# File:    test_security.py
+# Author:  Michael Cooney
+# Purpose: Define the Security class that will be used to hold, retrieve, and
+#          update information about stocks and ETFs.
+#
+################################################################################
+
 import datetime as dt
-from unittest.mock import patch
 import pandas as pd
 import sqlite3
 import unittest
 from unittest.mock import Mock, MagicMock, call
 import yfinance as yf
 
-import mcmc
-
-from mcmc import calculate_markov_chain, monte_carlo_sim, calculate_price_move_distribution
-
-from mcmc import Security
-
-
-SKIP_FETCH_UPDATED_DATA = False
-SKIP_PRICE_MOVE_DISTRIBUTION = True
-SKIP_CALCULATE_MARKOV_CHAIN = True
-SKIP_MONTE_CARLO_SIM = True
-
-
-# class MockDatetime(dt.datetime):
-    
-#     _fixed: dt.datetime = None
-
-#     @classmethod
-#     def today(self):
-#         return self._fixed
+from errors import NoNewDataError
+import security as security_module
+from security import Security
 
 
 class TestFetchUpdatedData(unittest.TestCase):
@@ -49,11 +40,10 @@ class TestFetchUpdatedData(unittest.TestCase):
 
         # save the original datetime module for teardown, replace the original
         # with the mock 
-        self._orig_dt = mcmc.dt
-        mcmc.dt = self.mock_dt_module
-
-
+        self._orig_dt = security_module.dt
+        security_module.dt = self.mock_dt_module
         
+
         ################################################################################
         # repeat the above for the sqlite3 connection
         ################################################################################
@@ -77,13 +67,14 @@ class TestFetchUpdatedData(unittest.TestCase):
         self.mock_cursor.fetchone.return_value = 'Mock con.cur.execute.fetchon function chain called'
 
         # replace the module sqlite3 con with our mock con
-        self._orig_sqlite3 = mcmc.sqlite3
-        mcmc.sqlite3 = self.mock_sqlite3
+        self._orig_sqlite3 = security_module.sqlite3
+        security_module.sqlite3 = self.mock_sqlite3
 
 
         ################################################################################
         # repeat the above for the yfinance module
         ################################################################################
+
         mock_yf_data = pd.DataFrame(columns = ['Date', 'Open', 'High', 'Low', \
                                                'Close', 'Adj Close', 'Volume', \
                                                'Dividends', 'Stock Splits'], \
@@ -103,20 +94,18 @@ class TestFetchUpdatedData(unittest.TestCase):
         self.mock_yf_ticker.history.return_value = mock_yf_data
 
         # save original and replace with mock
-        self._orig_yf = mcmc.yf
-        mcmc.yf = self.mock_yf
+        self._orig_yf = security_module.yf
+        security_module.yf = self.mock_yf
 
         return
 
     
     def tearDown(self):
-        mcmc.dt = self._orig_dt
-        mcmc.sqlite3 = self._orig_sqlite3
-        mcmc.yf = self._orig_yf
+        security_module.dt = self._orig_dt
+        security_module.sqlite3 = self._orig_sqlite3
+        security_module.yf = self._orig_yf
 
         
-    # @unittest.skip('Not implemented yet')
-    @unittest.skipIf(SKIP_FETCH_UPDATED_DATA, 'Test was not selected to run.')
     def test_fetch_data_no_start_date(self):
         '''Security.fetch_updated_data: check if the function retrieves new data
         when Security.start_date is not set'''
@@ -136,7 +125,6 @@ class TestFetchUpdatedData(unittest.TestCase):
         return
 
     
-    @unittest.skipIf(SKIP_FETCH_UPDATED_DATA, 'Test was not selected to run.')
     def test_fetch_data_with_start_date(self):
         '''Security.fetch_updated_data: check if the function retrieves subset of
         new data when Security.start_date is set'''
@@ -178,7 +166,7 @@ class TestFetchUpdatedData(unittest.TestCase):
         self.assertIsNone(pd.testing.assert_frame_equal(self.security.history, correct_history))
         return
     
-    @unittest.skipIf(SKIP_FETCH_UPDATED_DATA, 'Test was not selected to run.')
+
     def test_fetch_data_saturday_and_thursday(self):
         '''Security.fetch_updated_data: check if the function retrieves new data
         when the last updated date is Thursday and today is Saturday'''
@@ -202,7 +190,6 @@ class TestFetchUpdatedData(unittest.TestCase):
         return
 
 
-    @unittest.skipIf(SKIP_FETCH_UPDATED_DATA, 'Test was not selected to run.')
     def test_fetch_data_saturday_and_friday(self):
         '''Security.fetch_updated_data: check if the function returns an error
         when the last updated date is Friday and today is Saturday'''
@@ -211,7 +198,7 @@ class TestFetchUpdatedData(unittest.TestCase):
         self.mock_dt_module.datetime.today.return_value = dt.datetime(2025, 5, 3, 16, 0, 0)
         should_be_error = self.security.fetch_updated_data()
 
-        self.assertTrue(type(should_be_error) == mcmc.NoNewDataError)
+        self.assertTrue(type(should_be_error) == NoNewDataError)
         self.mock_cursor.execute.assert_called_once()
         self.mock_dt_module.datetime.today.assert_called()
 
@@ -220,7 +207,6 @@ class TestFetchUpdatedData(unittest.TestCase):
         return
 
 
-    @unittest.skipIf(SKIP_FETCH_UPDATED_DATA, 'Test was not selected to run.')
     def test_fetch_data_sunday_and_friday(self):
         '''Security.fetch_updated_data: check if the function returns an error
         when the last updated date is Friday and today is Sunday'''
@@ -229,7 +215,7 @@ class TestFetchUpdatedData(unittest.TestCase):
         self.mock_dt_module.datetime.today.return_value = dt.datetime(2025, 5, 4, 12, 0, 0)
         should_be_error = self.security.fetch_updated_data()
 
-        self.assertTrue(type(should_be_error) == mcmc.NoNewDataError)
+        self.assertTrue(type(should_be_error) == NoNewDataError)
         self.mock_cursor.execute.assert_called_once()
         self.mock_dt_module.datetime.today.assert_called()
 
@@ -238,7 +224,6 @@ class TestFetchUpdatedData(unittest.TestCase):
         return
 
 
-    @unittest.skipIf(SKIP_FETCH_UPDATED_DATA, 'Test was not selected to run.')
     def test_fetch_data_holiday(self):
         '''Security.fetch_updated_data: check if the function returns an error
         when today is a holiday and timedelta is less than one'''
@@ -247,7 +232,7 @@ class TestFetchUpdatedData(unittest.TestCase):
         self.mock_dt_module.datetime.today.return_value = dt.datetime(2025, 7, 4, 12, 0, 0)
         should_be_error = self.security.fetch_updated_data()
 
-        self.assertTrue(type(should_be_error) == mcmc.NoNewDataError)
+        self.assertTrue(type(should_be_error) == NoNewDataError)
         self.mock_cursor.execute.assert_called_once()
         self.mock_dt_module.datetime.today.assert_called()
 
@@ -256,7 +241,6 @@ class TestFetchUpdatedData(unittest.TestCase):
         return
 
 
-    @unittest.skipIf(SKIP_FETCH_UPDATED_DATA, 'Test was not selected to run.')
     def test_fetch_data_market_open(self):
         '''Security.fetch_updated_data: check if the function returns an error
         when today's market is still open and timedelta is less than one day'''
@@ -265,7 +249,7 @@ class TestFetchUpdatedData(unittest.TestCase):
         self.mock_dt_module.datetime.today.return_value = dt.datetime(2025, 5, 2, 12, 0, 0)
         should_be_error = self.security.fetch_updated_data()
 
-        self.assertTrue(type(should_be_error) == mcmc.NoNewDataError)
+        self.assertTrue(type(should_be_error) == NoNewDataError)
         self.mock_cursor.execute.assert_called_once()
         self.mock_dt_module.datetime.today.assert_called()
 
@@ -274,7 +258,6 @@ class TestFetchUpdatedData(unittest.TestCase):
         return
 
 
-    @unittest.skipIf(SKIP_FETCH_UPDATED_DATA, 'Test was not selected to run.')
     def test_fetch_data_same_date(self):
         '''Security.fetch_updated_data: check if the function returns an error
         when today and last_date are equal'''
@@ -283,7 +266,7 @@ class TestFetchUpdatedData(unittest.TestCase):
         self.mock_dt_module.datetime.today.return_value = dt.datetime(2025, 5, 1, 17, 0, 0)
         should_be_error = self.security.fetch_updated_data()
 
-        self.assertTrue(type(should_be_error) == mcmc.NoNewDataError)
+        self.assertTrue(type(should_be_error) == NoNewDataError)
         self.mock_cursor.execute.assert_called_once()
         self.mock_dt_module.datetime.today.assert_called()
 
@@ -293,110 +276,5 @@ class TestFetchUpdatedData(unittest.TestCase):
 
 
 
-class TestCalculatePriceMoveDistribution(unittest.TestCase):
-    @unittest.skipIf(SKIP_PRICE_MOVE_DISTRIBUTION, 'Test was not selected to run.')
-    def test_basic(self):
-        '''calculate_price_move_distribition: test a standard input'''
-
-        
-        data = pd.DataFrame(data=[10, 10.3, 10.8, 10.6, 10, 11, 12, 12, \
-                                  12.5, 12.25, 12.58, 12.18, 17.18, 14.18])
-
-        correct_answers = {'large gain': [5],
-                           'small gain': [0.3, 0.5, 1, 1, 0.5, 0.33],
-                           'no movement': [0],
-                           'small loss': [-0.2, -0.6, -0.25, -0.4],
-                           'large loss': [-3]}
-
-        bins = calculate_price_move_distribution(data)
-
-        for b in bins:
-            self.assertEqual(bins[b], correct_answers[b])
-
-
-class TestCalculateMarkovChain(unittest.TestCase):
-    @unittest.skipIf(SKIP_CALCULATE_MARKOV_CHAIN, 'Test was not selected to run.')
-    def test_basic(self):
-        '''calculate_markov_chain: check for expected result from model input'''
-        d = pd.DataFrame(data=[10, 10, 11, 12.5, 7.5, 4.5, 8.5, 13.5, 19.5, 14.75])
-        expected = [[0, 0, 1, 0, 0],\
-                    [0, 2/3, 0, 0, 1/3],\
-                    [0, 0, 1/2, 0, 1/2],\
-                    [0, 1, 0, 0, 0],\
-                    [0, 0, 0, 1, 0]]
-        result = calculate_markov_chain(d)
-        
-        for i in range(len(result)):
-            self.assertListEqual(list(result[i]), expected[i])
-        return
-
-    @unittest.skipIf(SKIP_CALCULATE_MARKOV_CHAIN, 'Test was not selected to run.')
-    def test_always_gain(self):
-        '''calculate_markov_chain: edge case when price data always increases (divide by 0)'''
-        d = pd.DataFrame(data=[10, 11, 12.5, 17.5, 20.5, 24.5, 29.5, 35.5, 40.25])
-        expected = [[0, 0, 0, 0, 0],\
-                    [0, 3/4, 1/4, 0, 0],\
-                    [0, 2/3, 1/3, 0, 0],\
-                    [0, 0, 0, 0, 0],\
-                    [0, 0, 0, 0, 0]]
-        result = calculate_markov_chain(d)
-
-        for i in range(len(result)):
-            self.assertListEqual(list(result[i]), expected[i])            
-        return
-
-    @unittest.skipIf(SKIP_CALCULATE_MARKOV_CHAIN, 'Test was not selected to run.')
-    def test_always_lose(self):
-        '''calculate_markov_chain: edge case when price data always decreases (divide by 0)'''
-        d = pd.DataFrame(data=[41, 40, 38.5, 33.5, 30.5, 26.5, 21.5, 15.5, 10.75])
-        expected = [[0, 0, 0, 0, 0],\
-                    [0, 0, 0, 0, 0],\
-                    [0, 0, 0, 0, 0],\
-                    [0, 0, 0, 1/3, 2/3],\
-                    [0, 0, 0, 1/4, 3/4]]
-        result = calculate_markov_chain(d)
-
-        for i in range(len(result)):
-            self.assertListEqual(list(result[i]), expected[i])            
-        return
-
-
-    @unittest.skipIf(SKIP_CALCULATE_MARKOV_CHAIN, 'Test was not selected to run.')
-    def test_no_movement(self):
-        '''calculate_markov_chain: edge case where the data never increases or decreases'''
-        d = pd.DataFrame(data=[10, 10, 10, 10, 10, 10])
-        expected = [[1, 0, 0, 0, 0],\
-                    [0, 0, 0, 0, 0],\
-                    [0, 0, 0, 0, 0],\
-                    [0, 0, 0, 0, 0],\
-                    [0, 0, 0, 0, 0]]
-        result = calculate_markov_chain(d)
-
-        for i in range(len(result)):
-            self.assertListEqual(list(result[i]), expected[i])
-        return
-
-class TestMonteCarloSim(unittest.TestCase):
-    @unittest.skipIf(SKIP_MONTE_CARLO_SIM, 'Test was not selected to run.')
-    def test_always_gain(self):
-        '''random_walk: edge case where markov chain has form [[1,0],[0,0]] '''
-        depth = 100
-        pts = monte_carlo_sim([[1,0], [0,0]], depth, 0)
-
-        # we cannot directly compare python lists, so we compare elementwise instead
-        verification = list(map(lambda x: x[0] == x[1], zip(pts, range(1, depth+1))))
-        self.assertTrue(all(verification))
-
-
-    @unittest.skipIf(SKIP_MONTE_CARLO_SIM, 'Test was not selected to run.')
-    def test_always_loss(self):
-        '''random_walk: edge case where markov chain has form [[0,0], [0,1]]'''
-        depth = 100
-        pts = monte_carlo_sim([[0,0], [0,1]], depth, 1)
-
-        verification = list(map(lambda x: x[0] == x[1], zip(pts, range(-1, -depth-1, -1))))
-        self.assertTrue(all(verification))
-        
 if __name__ == '__main__':
-    unittest.main(verbosity=3)
-
+    unittest.main(verbosity=1)
