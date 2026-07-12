c885193b1de922e9cea59b999cb07984099b3282
Michael <cooneymichael@outlook.com>
2026-07-11 20:38:20 -0400

    BUS-11: implemented n-day EMA

--- src/strategy.py
+++ src/strategy.py
@@ -12,8 +12,37 @@ from security import Security
 
 
 def n_day_sma(security: Security, num_days: int, window_size: int) -> pd.DataFrame:
-    start_day = dt.datetime.today() - dt.timedelta(days=num_days)
+    # start_day = dt.datetime.today() - dt.timedelta(days=num_days)
     # hist = security.get_history(start_day)
     hist = security.get_history()
     return hist['Close'].rolling(window=window_size, min_periods=window_size).mean()
 
+
+# def n_day_ema(security: Security, num_days: int, window_size: int, smoothing: int) -> pd.DataFrame:
+def n_day_ema(security: Security, window_size: int) -> pd.DataFrame:
+    # start_day = dt.datetime.today() - dt.timedelta(days=num_days)
+    # hist = security.get_history(start_day)
+
+    hist = security.get_history()
+    multiplier = 2 / (1 + window_size)
+    # initial ema is sma
+    init_ema = hist['Close'].iloc[:window_size].mean()
+
+    for idx, row in hist.iloc[window_size:].iterrows():
+        val_t = row.values[0]
+        ema = (val_t * multiplier) + (init_ema * (1-multiplier))
+        hist.loc[idx, 'ema'] = ema
+        init_ema = ema
+
+    return hist['ema']
+
+
+# def ema(df, span):
+#    ...:     multiplier = 2/(1+span)
+#    ...:     init_sma = df['Close'].iloc[:span].mean()
+#    ...:     for idx, row in df.iloc[span:].iterrows():
+#    ...:         val_t = row.values[0]
+#    ...:         ema = (val_t * multiplier) + (init_sma * (1-multiplier))
+#    ...:         df.loc[idx, 'ema'] = ema
+#    ...:         init_sma = ema
+#    ...:     print(df)
